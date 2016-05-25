a08c775a6b30f52ad17a98e524d42c1541ffce25
Marcus <mmond21@gmail.com>
2016-05-25 11:43:30 -0500

    updating navbar so links match pages

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -6,10 +6,10 @@
         <div class="nav-wrapper">
             <a href="#" class="brand-logo right">ADLISTEROFDEATH</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Items</a></li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Signup</a></li>
+                <li><a href="../views/ads/index.php">Home</a></li>
+                <li><a href="../views/ads/show.php">Items</a></li>
+                <li><a href="../views/ads/edit.php">Login</a></li>
+                <li><a href="../views/ads/create.php">Signup</a></li>
             </ul>
         </div>
 </nav>
