f19056e2807c29207b025d3fa9ba3918f3c1e697
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-25 11:44:16 -0500

    templated home landing page and added 404 to template

--- public/template.php
+++ public/template.php
@@ -1,6 +1,8 @@
 <?php 
 
-include_once '/..navbar.php';
+include_once __DIR__ . '/../views/partials/navbar.php';
+include_once __DIR__ . '/../views/home.php';
+include_once __DIR__ . '/../views/404.php';
 
  ?>
 <!doctype html>
