d85fe12d78252f69fc998092bf94546e32c9ed67
Marcus <mmond21@gmail.com>
2016-05-25 13:05:23 -0500

    bring back template file

---
+++ public/template.php
@@ -0,0 +1,22 @@
+<?php 
+
+include_once __DIR__ . '/../views/partials/head.php';
+include_once __DIR__ . '/../views/partials/navbar.php';
+// include_once __DIR__ . '/../views/404.php';
+include_once __DIR__ . '/../views/home.php';
+//connection to db
+include_once __DIR__ . '/../env.template.php';
+
+ ?>
+<!doctype html>
+<html>
+ <head>
+     
+     <title>Template</title>
+     
+ </head>
+ <body>
+     
+     
+ </body>
+</html>
