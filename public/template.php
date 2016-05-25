51b6ae6283e12b285c46cad566467e4d2a69bff5
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-25 15:07:50 -0500

    checking template and card size on home

--- public/template.php
+++ public/template.php
@@ -4,6 +4,8 @@ include_once __DIR__ . '/../views/partials/head.php';
 include_once __DIR__ . '/../views/partials/navbar.php';
 // include_once __DIR__ . '/../views/404.php';
 include_once __DIR__ . '/../views/home.php';
+include_once __DIR__ . '/../views/users/signup.php';
+include_once __DIR__ . '/../views/ads/create.php';
 //connection to db
 include_once __DIR__ . '/../env.template.php';
 
