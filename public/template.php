fa6cd47f9839fecd8f3a6fbea2d71f499f4d0248
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-26 10:08:30 -0500

    rough layouts of views complete

--- public/template.php
+++ public/template.php
@@ -3,12 +3,15 @@
 include_once __DIR__ . '/../views/partials/head.php';
 include_once __DIR__ . '/../views/partials/navbar.php';
 // include_once __DIR__ . '/../views/404.php';
-include_once __DIR__ . '/../views/home.php';
 include_once __DIR__ . '/../views/users/signup.php';
-include_once __DIR__ . '/../views/ads/create.php';
-include_once __DIR__ . '/../views/ads/edit.php';
-include_once __DIR__ . '/../views/ads/index.php';
-include_once __DIR__ . '/../views/ads/show.php';
+include_once __DIR__ . '/../views/users/edit.php';
+include_once __DIR__ . '/../views/users/login.php';
+include_once __DIR__ . '/../views/users/account.php';
+// include_once __DIR__ . '/../views/home.php';
+// include_once __DIR__ . '/../views/ads/create.php';
+// include_once __DIR__ . '/../views/ads/edit.php';
+// include_once __DIR__ . '/../views/ads/index.php';
+// include_once __DIR__ . '/../views/ads/show.php';
 //connection to db
 // include_once __DIR__ . '/../.env.php';
 
--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,44 @@
 <!--Page for user account home-->
+<!-- Page adjust navbar if user is logged in -->
+
+<!-- User info -->
+
+<div class="row">
+    <div class="col s4 offset-s4">
+        <p>grab username</p>
+        <p>grab email</p>
+    </div>
+</div>
+<!--  User edit profile button -->
+<div class="row">
+    <div class="col s4 offset-s4">
+        <a href="/edit" class="btn btn-success">Edit Profile</a>
+    </div>    
+</div>
+
+<!-- Users ads -->
+
+<div class="row">
+    <?php // foreach ($variable as $key => $value): ?>
+        <div class="card small col s6">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="/IMG/<?php //echo $value['name']?>.jpg">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4"><?php //echoInput::escape($value['name'])?><i class="material-icons right">More Info</i></span>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4"><?php //echoInput::escape($value['name'])?><i class="material-icons right">X</i></span>
+                <p>Data: <?php //echoInput::escape($value['location'])?> </p>
+                <p>Data:  <?php //echoInput::escape($value['date_established'])?> </p>
+                <p>Data: <?php //echoInput::escape($value['area_in_acres'])?> </p>
+                <p>Data: <?php //echoInput::escape($value['description'])?> </p>
+            </div>
+        </div>
+    <?php// endforeach; ?>
+    </div>
+<div class="row">
+    <div class="col-sm-6 text-right">
+        <a href="/create" class="btn btn-success">Create Ad</a>
+    </div>
+</div>
