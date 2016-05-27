ee1fec1d712fdc8a79f3d5d067f28a806490689d
Marcus <mmond21@gmail.com>
2016-05-27 14:31:18 -0500

    finished up index, home, and item pages. still working on css stuff.

--- views/ads/show.php
+++ views/ads/show.php
@@ -1,31 +1,28 @@
 <!--Page for single advertisement -->
 <?php 
 
+$uniqueId = implode(",", $_GET);
+$uniqueId = intval($uniqueId);
+$var = Item::find($uniqueId);
+// var_dump($var);
+
 ?>
 
 <div class="row">
-    <div class="col s6">
-        <div class="row">
-            <div class="col s12">
-                <div class="card medium">
-                    <div class="card-image">
-                        <img src="/css/skull.png">
-                        <span class="card-title">Card Title</span>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">This is a link</a>
-                    </div>
-                </div>
+    <div class="col s6 m6 l6">
+        <div id ="cardz" class="card">
+            <div class="container card-image">
+                <p>Description: <?=($var->name) ?> </p>
+                <p>Price: <?=($var->price) ?> </p>
+                <p>Seller:  <?=($var->owner) ?> </p>
+                <img src="/img/<?php echo($var->picture) ?>">
             </div>
         </div>
     </div>
-    
-    <div class="col s6">
-        <span class="flow-text">Description</span>
-    </div>    
 </div>
-<div class="row">
+
 <!-- If logged in show -->
+<div class="row">
     <div class="col s6 left-align">
         <a href="/edit" class="btn btn-success">Edit Ad</a>
     </div>
