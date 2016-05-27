ced37c97fcee486b27d20da1c9e149e6c70eb7ba
Marcus <mmond21@gmail.com>
2016-05-27 10:15:16 -0500

    updates to login/signup pages

--- views/users/login.php
+++ views/users/login.php
@@ -1,50 +1,43 @@
-<div class="container">
-
-	<section id="login">
-
-		<div class="row loginUser">
-
-			<h1 class="section-title">Login To AdLister of Death</h1>
-
-			<?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
-                <div class="alert alert-danger">
-                    <p class="error"><?= $_SESSION['ERROR_MESSAGE']; ?></p>
-                </div>
+<div class="row">
+	<div class="col s12 z-depth-6 card-panel">
+        <section id="login">
+            <div class="row loginUser">
+                <h1 class="section-title" style="text-align: center">We knew you'd return...</h1>
+                <?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
+                    <div class="alert alert-danger">
+                        <p class="error"><?= $_SESSION['ERROR_MESSAGE']; ?></p>
+                    </div>
                 <?php unset($_SESSION['ERROR_MESSAGE']); ?>
-            <?php endif; ?>
-            <?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
-                <div class="alert alert-success">
-                    <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
-                </div>
-                <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
-            <?php endif; ?>
-
-			<div class="col-md-6 col-md-offset-3">
-
-				<p>Login with your email/username and password</p>
-
+                <?php endif; ?>
+                <?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
+                    <div class="alert alert-success">
+                        <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
+                    </div>
+                    <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
+                <?php endif; ?>
+    			<p>Login with your email/username and password</p>
 				<form method="POST" action="" data-validation data-required-message="This field is required">
-
-					<div class="form-group">
-					    <input type="text" class="form-control" id="email_user" name="email_user" placeholder="Email or Username" data-required>
+					<div class="row margin">
+                        <div class="input-field col s12">
+					       <input type="text" class="form-control" id="email_user" name="email_user" placeholder="Email or Username" data-required>
+                        </div>
 					</div>
-					<div class="form-group">
-					    <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
+                    <div class="row margin">
+                        <div class="input-field col s12">
+					       <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
+                        </div>
 					</div>
 					<div class="row">
 						<div class="col-sm-6">
-							<button type="submit" class="btn btn-primary">Login</button>
+							<button type="submit" class="waves-effect waves-black btn-flat">Login</button>
 						</div>
-						<div class="col-sm-6 text-right">
-							<a href="/signup" class="btn btn-success">Go To Signup</a>
-						</div>
+                        <br>
+						<!-- <div class="col-sm-6">
+							<button class="loginUser waves-effect waves-black btn-flat">Go To Signup</button>
+						</div> -->
 					</div>
 				</form>
-
-			</div>
-
-		</div>
-
-	</section>
-
+    		</div>
+        </section>
+    </div>
 </div>
